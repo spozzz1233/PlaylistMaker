05330e192756bae3dfe73fe8d33fc4ce60a10f8f
spozzz1233 <minulin.misha@bk.ru>
2023-03-02 16:51:01 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search = findViewById<Button>(R.id.search)
+        val media = findViewById<Button>(R.id.media)
+        val settings = findViewById<Button>(R.id.settings)
+        search.setOnClickListener {
+            val searchIntent = Intent(this,SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        media.setOnClickListener {
+            val mediatekaIntent = Intent(this,MediatekaActivity::class.java)
+            startActivity(mediatekaIntent)
+        }
+        settings.setOnClickListener {
+            val settingsIntent = Intent(this,SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
     }
 }
