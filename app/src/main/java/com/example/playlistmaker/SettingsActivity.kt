67d53e46e5d8db267500602b4dc46d3a8475e969
spozzz1233 <minulin.misha@bk.ru>
2023-03-10 17:26:43 +0300

    revision

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,18 +1,41 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val back = findViewById<ImageView>(R.id.back)
+        val share = findViewById<TextView>(R.id.share)
+        val theme = findViewById<TextView>(R.id.darkTheme)
+        val support = findViewById<TextView>(R.id.Support)
+        val terms = findViewById<TextView>(R.id.terms)
         back.setOnClickListener {
-            val backIntent = Intent(this,MainActivity::class.java)
-            startActivity(backIntent)
+            finish()
         }
+        share.setOnClickListener{
+            val message = "Привет, приходи к нам на курс 'https://practicum.yandex.ru/profile/android-developer/'"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("minulin.misha@bk.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+        theme.setOnClickListener{
+
+        }
+        support.setOnClickListener{
+
+        }
+        terms.setOnClickListener{
+
+        }
+
     }
 }
