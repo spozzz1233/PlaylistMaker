3baf0743180c3af2e9cfed29335ad59d7bdce3f8
spozzz1233 <minulin.misha@bk.ru>
2023-03-13 21:57:37 +0300

    init commit

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,63 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    var inputText:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val back = findViewById<ImageView>(R.id.back)
+        val clearButton = findViewById<ImageView>(R.id.clear)
+        var inputEditText = findViewById<EditText>(R.id.SearchForm)
+        back.setOnClickListener {
+            finish()
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+        inputEditText.setText(inputText)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        var inputEditText = findViewById<EditText>(R.id.SearchForm)
+        outState.putString(TEXT_SEARCH, inputEditText.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputText = savedInstanceState.getString(TEXT_SEARCH)
+
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+    companion object {
+        const val TEXT_SEARCH = "TEXT_SEARCH"
+    }
+
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +20,11 @@ class SettingsActivity : AppCompatActivity() {
         val theme = findViewById<TextView>(R.id.darkTheme)
         val support = findViewById<TextView>(R.id.Support)
         val terms = findViewById<TextView>(R.id.terms)
+
         back.setOnClickListener {
             finish()
         }
+
         share.setOnClickListener{
             val message = "Привет, приходи к нам на курс 'https://practicum.yandex.ru/profile/android-developer/'"
             val shareIntent = Intent(Intent.ACTION_SENDTO)
@@ -28,14 +34,27 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(shareIntent)
         }
         theme.setOnClickListener{
-
+            val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when (currentNightMode) {
+                Configuration.UI_MODE_NIGHT_NO -> {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)} // Night mode is not active, we're using the light theme.
+                Configuration.UI_MODE_NIGHT_YES -> {AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)} // Night mode is active, we're using dark theme.
+            }
         }
         support.setOnClickListener{
-
+            val message1 = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message2 = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("minulin.misha@bk.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message2)
+            shareIntent.putExtra(Intent.EXTRA_TITLE, message1)
+            startActivity(shareIntent)
         }
         terms.setOnClickListener{
-
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(browserIntent)
         }
+
 
     }
 }
